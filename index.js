f28b01dfd0ac8961a729ac0cd5acd5d45926b676
Nikhil <nikhilyadav3000@gmail.com>
2019-11-30 00:06:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+var map1 = new Map([["1" , "8"], ["2" ,"7" ] ,["3", "15"],["4" , "20"], ["5" ,"35" ] ,["6", "25"]]); 
+  
+
+
+app.get('/:flightId',(req,res)=>{
+    var id = req.params.flightId;
+    if(map1.has(id))
+    {
+        console.log(map1.get(id));
+        res.send({
+            'status' : 'found',
+            'delay' : map1.get(id),
+            'timestamp' : Date.now()
+        });
+    }
+    else{
+        res.send({'status': 'not found'});
+    }
+})
+
+var port = process.env.port || 5000;
+
+app.listen(port,()=>{
+    console.log("listening at port" + port);
+})
